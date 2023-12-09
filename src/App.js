abf636bfb7c5bc137643ff4b86e7d8a32113b4de
Alina Pavalachi <al.el.en.lina@gmail.com>
2023-12-10 01:01:42 +0200

    adding project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import btnImg from './assets/images/btn-img.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='card'>
+
+        <div className='title'>ADVICE #117</div>
+
+        <div className='content'>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</div>
+
+        <div className='divider'>
+          <div className='divider-line'></div>
+          <div className='divider-par'>
+            <div className='line'></div>
+            <div className='line'></div>
+          </div>
+          <div className='divider-line'></div>
+        </div>
+
+        <div className='btn'>
+          <img className='btn-img' alt='btn-img' src={btnImg}/>
+        </div>
+      </div>
     </div>
   );
 }
